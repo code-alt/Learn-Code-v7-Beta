786ec7e6b5f097407c867d8f7ed0a77d56d60d26
code-alt <74145728+code-alt@users.noreply.github.com>
2021-11-23 17:25:41 -0600

    rollback

--- .history/static/script_20211122223249.js
+++
@@ -1,3 +0,0 @@
-const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
-const buttonRipple = MDCRipple.attachTo(document.querySelector('#btn1'));
-const buttonRipple = MDCRipple.attachTo(document.querySelector('#btn2'));
--- .history/static/script_20211123154212.js
+++
@@ -1,6 +0,0 @@
-import "https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js";
-
-const selector = '.mdc-button, .mdc-icon-button, .mdc-card__primary-action';
-const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
-  return new MDCRipple(el);
-});
